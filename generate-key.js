02e31b519a6bcd19372814d3a8d8c5ad29a44350
mack4pf <mackiyeritufu@gmail.com>
2026-03-11 23:52:13 +0100

    FIXED BUG

--- generate-key.js
+++
@@ -1,4 +0,0 @@
-const crypto = require('crypto');
-const key = crypto.randomBytes(32).toString('hex');
-console.log('Add this to your .env file:');
-console.log(`ENCRYPTION_KEY=${key}`);
--- src/test-env.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-console.log('🔍 TESTING ENVIRONMENT VARIABLES');
-console.log('='.repeat(50));
-console.log('ADMIN_CHAT_ID from .env:', process.env.ADMIN_CHAT_ID);
-console.log('Length:', process.env.ADMIN_CHAT_ID?.length);
-console.log('Characters:', JSON.stringify(process.env.ADMIN_CHAT_ID));
-console.log('='.repeat(50));
